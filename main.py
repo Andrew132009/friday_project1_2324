a524c93c805a00bdf452a277ad89b776503e0aa8
Andrew <abekarevich2009@gmail.com>
2023-09-29 11:23:32 -0400

    Adding game settings

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import sys
 import pygame as pg
+from settings import Settings
 
-W = 1280
-H = 720
 
-pg.init()  # инициализируем pygame
-screen = pg.display.set_mode((W, H))  # создаем экран игры разрешением 1280х720px
+def run_game():
+    ai_settings = Settings()
+    pg.init()  # инициализируем pygame
+    screen = pg.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))  # создаем экран игры разрешением 1280х720px
+    pg.display.set_caption("Alien Invasion")
+    while True:  # цикл игры
+        for event in pg.event.get():  # обработчик событий pygame
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
 
-while True:  # цикл игры
-    for event in pg.event.get():  # обработчик событий pygame
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
+        screen.fill(ai_settings.bg_color)
+        pg.display.flip()
+
+
+run_game()
